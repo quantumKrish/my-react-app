781aa63a654d25f3156ca2abf1edf560a6340089
quantumKrish <kirishanth290@gmail.com>
2025-03-13 12:38:54 +0000

    created a new file to play around with es6 since apparently i should know how it works

--- new-app/src/index.js
+++ new-app/src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom/client';
 const myFirstElement = <h1>Hello React!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);        
+root.render(myFirstElement);
